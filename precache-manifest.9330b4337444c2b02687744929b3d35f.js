79fe988a78234558e6f0f665b4f10a84539c79a8
Reginald Galang <reguv760@gmail.com>
2019-04-19 10:26:27 -0700

    Updates

---
+++ precache-manifest.9330b4337444c2b02687744929b3d35f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "96495d5f2625f137868c",
+    "url": "/static/js/main.0ffc025f.chunk.js"
+  },
+  {
+    "revision": "225418a53b9a0b1eadbd",
+    "url": "/static/js/2.327377a8.chunk.js"
+  },
+  {
+    "revision": "96495d5f2625f137868c",
+    "url": "/static/css/main.49b0fef1.chunk.css"
+  },
+  {
+    "revision": "afbf2de59b1b917695107ba525ce4d00",
+    "url": "/index.html"
+  }
+];
