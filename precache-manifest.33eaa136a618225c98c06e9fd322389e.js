853671af64e97bf72a4bdffc8e0d1166ad811f98
Reginald Galang <reguv760@gmail.com>
2019-04-19 10:35:33 -0700

    Updates

---
+++ precache-manifest.33eaa136a618225c98c06e9fd322389e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e3818cd1cd8155d301c",
+    "url": "/multistepform-react/static/js/runtime~main.29abad69.js"
+  },
+  {
+    "revision": "9b099eb2345860ab19f3",
+    "url": "/multistepform-react/static/js/main.adbc8b22.chunk.js"
+  },
+  {
+    "revision": "225418a53b9a0b1eadbd",
+    "url": "/multistepform-react/static/js/2.327377a8.chunk.js"
+  },
+  {
+    "revision": "9b099eb2345860ab19f3",
+    "url": "/multistepform-react/static/css/main.49b0fef1.chunk.css"
+  },
+  {
+    "revision": "7d38b5d4dbf0934b5b8f6e938121487c",
+    "url": "/multistepform-react/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9330b4337444c2b02687744929b3d35f.js"
+  "/multistepform-react/precache-manifest.33eaa136a618225c98c06e9fd322389e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/multistepform-react/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
